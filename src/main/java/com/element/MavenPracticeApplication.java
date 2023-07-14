14c803e1122f704497e48649bbdca72b62685a69
madhuridhikale74 <dhikalemadhuri74@gmail.com>
2023-07-14 12:18:43 +0530

    create file

---
+++ src/main/java/com/element/MavenPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/element/MavenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
